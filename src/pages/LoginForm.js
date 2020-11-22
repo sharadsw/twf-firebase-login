7bce1cc9567fea61e1dc279544aca6de2c54543d
TheRealSS <sharad97@gmail.com>
2020-11-23 04:20:30 +0530

    Minor formatting changes

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,27 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
 
   return (
-    <div>
+    <div className="container is-max-desktop">
+      <h1 className="title">Log in to your account</h1>
 
-    <div className="field">
-      <label className="label">Name</label>
-      <div className="control">
-        <input className="input" type="text" placeholder="e.g John Smith" />
+      <div className="field">
+        <label className="label">Email</label>
+        <div className="control">
+          <input className="input" type="text" placeholder="e.g. johnsmith@gmail.com" />
+        </div>
       </div>
-    </div>
 
-    <div className="field">
-      <label className="label">Email</label>
-      <div className="control">
-        <input className="input" type="text" placeholder="e.g. johnsmith@gmail.com" />
+      <div className="field">
+        <label className="label">Password</label>
+        <div className="control">
+          <input className="input" type="password" placeholder="Password" />
+        </div>
       </div>
-    </div>
 
-    <div class="control">
-      <button class="button is-success">Submit</button>
-    </div>
+      <div class="control">
+        <button class="button is-success">Log in</button>
+      </div>
+      <p className="help">
+        Haven't registered yet? Click <Link to="/signup">here</Link> to sign up.
+    </p>
 
     </div>
   )
