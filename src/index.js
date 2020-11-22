0909d2b134e1d13306d9e00a5e379e3e20455745
TheRealSS <sharad97@gmail.com>
2020-11-23 04:20:55 +0530

    Add routes to pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import {
 } from "react-router-dom";
 
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 import LoginForm from './pages/LoginForm'
+import SignupForm from './pages/SignupForm'
+import Profile from './pages/Profile'
 
 import 'bulma/css/bulma.min.css'
 
@@ -16,7 +19,21 @@ const App = () => {
   return (
     <Router>
       <Navbar />
-      <LoginForm />
+      
+      <Switch>
+        <Route path="/signup">
+          <SignupForm />
+        </Route>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   )
 }
