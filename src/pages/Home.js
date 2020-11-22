380da9875ee6ff83627ab1cc9d371cd698c7faba
TheRealSS <sharad97@gmail.com>
2020-11-23 04:19:35 +0530

    Add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  
+  return (
+    <div className="container is-max-desktop">
+      <h1 className="title">Hello there!</h1>
+      <h2 className="subtitle">Welcome to the site</h2>
+    </div>
+  )
+}
+
+export default Home
