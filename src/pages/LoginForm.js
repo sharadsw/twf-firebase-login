a92a8ebce78e3b00ae430eeaf40b11d8ff2e74d3
TheRealSS <sharad97@gmail.com>
2020-11-24 05:47:09 +0530

    Finish signup and login

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,32 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
-const LoginForm = () => {
+const LoginForm = ({ user, handleLogIn }) => {
+
+  if (user) {
+    return ( <Redirect to="/profile" /> )
+  }
 
   return (
     <div className="container is-max-desktop">
       <h1 className="title">Log in to your account</h1>
 
-      <div className="field">
-        <label className="label">Email</label>
-        <div className="control">
-          <input className="input" type="text" placeholder="e.g. johnsmith@gmail.com" />
+      <form onSubmit={handleLogIn}>
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="e.g. johnsmith@gmail.com" name="loginmail" />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Password</label>
-        <div className="control">
-          <input className="input" type="password" placeholder="Password" />
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="Password" name="loginpass" />
+          </div>
         </div>
-      </div>
 
-      <div class="control">
-        <button class="button is-success">Log in</button>
-      </div>
-      <p className="help">
-        Haven't registered yet? Click <Link to="/signup">here</Link> to sign up.
-    </p>
+        <div class="control">
+          <button class="button is-success" type="submit">Log in</button>
+        </div>
+        <p className="help">
+          Haven't registered yet? Click <Link to="/signup">here</Link> to sign up.
+        </p>
+      </form>
 
     </div>
   )
--- src/pages/SignupForm.js
+++ src/pages/SignupForm.js
@@ -1,43 +1,42 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-const SignupForm = () => {
+const SignupForm = ({ user, handleSignUp }) => {
+
+  if (user) {
+    return ( <Redirect to="/profile" /> )
+  }
 
   return (
     <div className="container is-max-desktop">
       <h1 className="title">Register an account</h1>
 
-      <div className="field">
-        <label className="label">Username</label>
-        <div className="control">
-          <input className="input" type="text" placeholder="Username" />
+      <form onSubmit={handleSignUp}>
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="e.g. johnsmith@gmail.com" name="email" />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Email</label>
-        <div className="control">
-          <input className="input" type="text" placeholder="e.g. johnsmith@gmail.com" />
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="Password" name="pass" />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Password</label>
-        <div className="control">
-          <input className="input" type="password" placeholder="Password" />
+        <div className="field">
+          <label className="label">Confirm Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="Confirm Password" name="pass2" />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Confirm Password</label>
         <div className="control">
-          <input className="input" type="password" placeholder="Confirm Password" />
+          <button className="button is-link" type="submit">Sign up</button>
         </div>
-      </div>
-
-      <div className="control">
-        <button className="button is-link">Sign up</button>
-      </div>
-
+      </form>
     </div>
   )
 }
